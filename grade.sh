a8ee60f0e6a1bf89d9de46675e0f8cbe9e48a9bd
khushikumra <khushikumra@gmail.com>
2023-11-27 11:34:35 -0800

    updating

--- grade.sh
+++ grade.sh
@@ -48,11 +48,3 @@ if [ $? -eq 0 ]; then
 else 
     echo "Tests failed. Review the feedback above."
 fi
-
-
-
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
-
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
